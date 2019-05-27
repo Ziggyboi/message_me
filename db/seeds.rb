94349f975fc0814956ebf232ed1c9fd7ec687703
Ziggyboi <dumbleshoot@gmail.com>
2019-05-27 20:47:39 +0100

    Adding user and message model to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "John", password: "password")
+User.create(username: "Bitchname123", password: "password")
+User.create(username: "Jesus", password: "password")
+User.create(username: "Joseph", password: "password")
+User.create(username: "Rodrigo", password: "password")
+
+
+Message.create(body: "test Message 1", user: User.last)
+Message.create(body: "test Message 2", user: User.last)
+Message.create(body: "test Message 3", user: User.first)
+Message.create(body: "test Message 4", user: User.first)
+
