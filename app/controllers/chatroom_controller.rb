2a1c265a4595a30a8125dbc1aa029e2c306ab5f8
Ziggyboi <dumbleshoot@gmail.com>
2019-05-28 02:05:47 +0100

    translating View forms to emb ruby and adding WebSocket functionality

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,8 @@
 class ChatroomController < ApplicationController
-    def index
-        @messages = Message.all
+    before_action :require_user
 
+    def index
+        @message = Message.new
+        @messages = Message.all
     end
 end
